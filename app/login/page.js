75f7742a602baa44b9aefc9a91854be51c26e606
Minahil Ashraf <shazilashraf994@gmail.com>
2025-10-29 00:30:17 +0500

    updated

--- app/login/page.js
+++ app/login/page.js
@@ -569,7 +569,7 @@ const EnhancedLoginPage = ({ onAuthSuccess }) => {
                 className="text-white/60 hover:text-white/80 text-sm transition-colors"
                 onClick={() => {
                   // Handle forgot password
-                  console.log('Forgot password clicked')
+                  // console.log('Forgot password clicked')
                 }}
               >
                 Forgot your password?
